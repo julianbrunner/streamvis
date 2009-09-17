7462bfe282cded79cfb0d33bde75c75867defa95
Julian Brunner <julianbrunner@gmail.com>
2009-09-17 14:20:26 +0000

    Reenabled graph extension.

--- Visualizer.Plotting/Graph.cs
+++ Visualizer.Plotting/Graph.cs
@@ -42,56 +42,40 @@ namespace Visualizer.Plotting
 				ValueRange valueRange = valueManager.Range;
 
 				foreach (DataSegment segment in segmentManager[this])
-				{
-					TimeRange timeRange = segment.TimeRange;
-
-					float[] vertices = new float[segment.Entries.Length * 2];
-
-					int position = 0;
-					foreach (Entry entry in segment.Entries)
+					if (segment.Entries.Length > 0)
 					{
-						vertices[position++] = (float)entry.Time.Seconds;
-						vertices[position++] = (float)entry.Value;
+						TimeRange timeRange = segment.TimeRange;
+
+						int vertexCount = segment.Entries.Length;
+
+						if (segmentManager.ExtendGraphs) vertexCount += 2;
+
+						float[] vertices = new float[vertexCount * 2];
+						int position = 0;
+
+						if (segmentManager.ExtendGraphs)
+						{
+							vertices[position++] = (float)timeRange.Range.Start.Seconds;
+							vertices[position++] = (float)segment.Entries[0].Value;
+						}
+
+						foreach (Entry entry in segment.Entries)
+						{
+							vertices[position++] = (float)entry.Time.Seconds;
+							vertices[position++] = (float)entry.Value;
+						}
+
+						if (segmentManager.ExtendGraphs)
+						{
+							vertices[position++] = (float)timeRange.Range.End.Seconds;
+							vertices[position++] = (float)segment.Entries[segment.Entries.Length - 1].Value;
+						}
+
+						Matrix4 transformation = valueRange.Transformation * timeRange.Transformation * layouter.Transformation;
+
+						drawer.DrawLineStrip(vertices, transformation, Color, 1);
 					}
-
-					Matrix4 transformation = valueRange.Transformation * timeRange.Transformation * layouter.Transformation;
-
-					drawer.DrawLineStrip(vertices, transformation, Color, 1);
-				}
 			}
 		}
-
-		// TODO: Reenable graph extension
-		//IEnumerable<Entry> GetEntries(Time startTime, Time endTime)
-		//{
-		//    if (stream.Container.IsEmpty) yield break;
-
-		//    int startIndex = stream.Container.GetIndex(start);
-		//    int endIndex = stream.Container.GetIndex(end);
-
-		//    if (plotter.ExtendGraphs)
-		//    {
-		//        double head;
-		//        double tail;
-
-		//        if (startIndex == endIndex)
-		//        {
-		//            if (endIndex == stream.Container.Count) head = tail = stream.Container[endIndex - 1].Value;
-		//            else head = tail = stream.Container[startIndex].Value;
-		//        }
-		//        else
-		//        {
-		//            head = stream.Container[startIndex].Value;
-		//            tail = stream.Container[endIndex - 1].Value;
-		//        }
-
-		//        yield return new Entry(start, head);
-		//        foreach (Entry entry in stream.Container[startIndex, endIndex]) yield return entry;
-		//        yield return new Entry(end, tail);
-		//    }
-		//    else
-		//        foreach (Entry entry in stream.Container[startIndex, endIndex])
-		//            yield return entry;
-		//}
 	}
 }
