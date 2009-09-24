dc36346db6bff58e198c428f569117b0be478a67
Julian Brunner <julianbrunner@gmail.com>
2009-09-24 08:32:58 +0000

    Moved help message to an xml resource.

--- Source/Visualizer/Program.cs
+++ Source/Visualizer/Program.cs
@@ -16,8 +16,12 @@
 // along with Stream Visualizer.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using Utility;
 
 namespace Visualizer
@@ -32,139 +36,24 @@ namespace Visualizer
 		{
 			if (args.Contains("--help"))
 			{
-				const int column1 = 0;
-				const int column2 = 2;
-				const int column3 = 6;
-				const int column4 = 25;
+				using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Visualizer.Text.Help Message.xml"))
+				using (TextReader reader = new StreamReader(stream))
+				{
+					XElement helpMessage = XElement.Load(reader);
 
-				#region Help Message
-				Console.WriteLine();
+					IDictionary<string, int> columns =
+					(
+						from column in helpMessage.Element("Columns").Elements()
+						select new { Name = (string)column.Element("Name"), Value = (int)column.Element("Value") }
+					)
+					.ToDictionary(column => column.Name, column => column.Value);
 
-				ConsoleUtility.Write(column1, "Stream Visualizer is an application that can be used to visualize, capture and replay data streams.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column1, "Parameters:");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "<port>");
-				ConsoleUtility.Write(column4, "Specifies that all streams of <port> should be included in the stream selection list. A test bottle will be retrieved from the specified port to build a list of possible streams before the visualization begins.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "<port>:<streams>");
-				ConsoleUtility.Write(column4, "Specifies that the <streams> of <port> should be included in the stream selection list.");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "<streams>");
-				ConsoleUtility.Write(column4, "<range>,<range>,...");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "<range>");
-				ConsoleUtility.Write(column4, "<path>|<path>-<path>");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "<path>");
-				ConsoleUtility.Write(column4, "A path to the stream, subpaths are seperated by dots. For example \"1.2.3\" specifies that the stream should be generated from the third value in the second bottle in the first bottle of the port.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "+m|-m");
-				ConsoleUtility.Write(column4, "Enables or disables minimal mode. In minimal mode, everything except for the graph area is hidden from the user interface to maximize the available drawing-space. The default values is disabled.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "+e|-e");
-				ConsoleUtility.Write(column4, "Enables or disables the graph extension feature. When graph extension is enabled, all graphs that have at least one sampled data entry are extended across the whole width of the coordinate system using known adjacent values. The default values is enabled.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "+a|-a");
-				ConsoleUtility.Write(column4, "Enables or disables antialiasing of graph lines. The default values is enabled.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "+b|-b");
-				ConsoleUtility.Write(column4, "Enables or disables alpha-blending. The default values is enabled.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "+v|-v");
-				ConsoleUtility.Write(column4, "Enables or disables vertical synchronization. The default values is enabled.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "+l|-l");
-				ConsoleUtility.Write(column4, "Enables or disables the logging of all received data. Disable if you plan to run the visualizer for a long time and you don't want to save the results. The default values is enabled.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "w:<width>");
-				ConsoleUtility.Write(column4, "Lets you specify the width of the drawing-area in total seconds. The default value is \"10\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "l:<width>");
-				ConsoleUtility.Write(column4, "Lets you specify the width (thickness) of the graph lines in pixels. The default value is \"1.0\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "t:(c|s:[<c>]|w:[<c>])");
-				ConsoleUtility.Write(column4, "Lets you specify the type of diagram that is used. The default value is \"c\".");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "c");
-				ConsoleUtility.Write(column4, "Continuous diagram. The graphs are drawn across the entire drawing-area, the most recent data entry lies on the right border of the drawing-area.");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "s:[<c>]");
-				ConsoleUtility.Write(column4, "Shifting diagram. The graphs will shift <c> times the drawing-area width to the left whenever the most recent data entry reaches the right border of the drawing-area. The default value for <c> is \"0.8\".");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "w:[<c>]");
-				ConsoleUtility.Write(column4, "Wrapping diagram. The graphs don't move at all, once the most recent data entry reaches the right border, the graphs wrap around and draw over the oldest entries starting from the left. The most recent data entry will push a gap of <c> times the drawing-area width in front of it. The default value for <c> is \"0.2\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "r:<low>:<high>");
-				ConsoleUtility.Write(column4, "Fixes the value range of the diagram. By default, the value range is automatically fitted to the displayed graphs.");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "s:(s:[<c>]|p:[<c>])");
-				ConsoleUtility.Write(column4, "Lets you specify the sampler type and settings used for drawing graphs. The default value is \"p:1\".");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "s:[<c>]");
-				ConsoleUtility.Write(column4, "Per-Second sampler. The data will be resampled to <c> entries per second. The default value for <c> is \"10\".");
-				Console.WriteLine();
-				Console.WriteLine();
-				ConsoleUtility.Write(column3, "p:[<c>]");
-				ConsoleUtility.Write(column4, "Per-Pixel sampler. The data will be resampled to <c> entries per pixel. The default value for <c> is \"0.1\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "mx:<n>");
-				ConsoleUtility.Write(column4, "Sets the number of markers that are displayed on the X-Axis. The default value is \"5\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "my:<n>");
-				ConsoleUtility.Write(column4, "Sets the number of markers that are displayed on the Y-Axis. The default value is \"5\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "pc:<color>");
-				ConsoleUtility.Write(column4, "Sets the diagram color (coordinate system and labels) in HTML notation. The default value is \"FFFFFF\".");
-				Console.WriteLine();
-				Console.WriteLine();
-
-				ConsoleUtility.Write(column2, "bc:<color>");
-				ConsoleUtility.Write(column4, "Sets the background color in HTML notation. The default value is \"000000\".");
-				Console.WriteLine();
-				Console.WriteLine();
-				#endregion
+					foreach (XElement textElement in helpMessage.Element("Data").Elements())
+					{
+						if (textElement.Name == "Text") ConsoleUtility.Write(columns[(string)textElement.Attribute("Column")], (string)textElement);
+						if (textElement.Name == "NewLine") Console.WriteLine();
+					}
+				}
 
 				return;
 			}
