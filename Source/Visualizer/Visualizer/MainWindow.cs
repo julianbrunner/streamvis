c706eea342a67a70dd5dc8dba2c2cd66e19a1de1
Julian Brunner <julianbrunner@gmail.com>
2010-06-14 16:26:25 +0000

    Improved capture creation error message.

--- Source/Visualizer/Visualizer/MainWindow.cs
+++ Source/Visualizer/Visualizer/MainWindow.cs
@@ -342,7 +342,7 @@ namespace Visualizer
 			session = new Session(timer, Enumerable.Empty<string>());
 
 			try { session = new Session(timer, portStrings); }
-			catch (Exception e) { MessageBox.Show(e.Message, "Capture creation error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+			catch (Exception e) { MessageBox.Show(e.Message + "\n\n" + "Stacktrace:" + "\n" + e.StackTrace, string.Format("Capture creation error ({0})", e), MessageBoxButtons.OK, MessageBoxIcon.Error); }
 
 			RebuildList();
 		}
