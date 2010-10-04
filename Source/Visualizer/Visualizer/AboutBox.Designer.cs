e29074da42078f65ed728c1c61acb72d08677f5e
Julian Brunner <julianbrunner@gmail.com>
2010-10-04 14:59:27 +0000

    Removed BOMs.

--- Source/Visualizer/Visualizer/AboutBox.Designer.cs
+++ Source/Visualizer/Visualizer/AboutBox.Designer.cs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Stream Visualizer.  If not, see <http://www.gnu.org/licenses/>.
 
-﻿namespace Visualizer
+namespace Visualizer
 {
 	partial class AboutBox
 	{
